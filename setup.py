2a787114b158fe741329b88d1f44da4316ef4c18
Deniz Bozyigit <dbozyigit@battrion.com>
2020-09-28 12:11:57 +0200

    new version

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-def fixed_version(*args, **kwds):
-    return '0.15.1'
-
-def no_tag(*args, **kwds):
-    return ''
-
-
 if __name__ == "__main__":
-    conf = dict(use_scm_version=dict(
-            version_scheme=fixed_version,
-            local_scheme=no_tag)
-        )
-    setup(**conf)
+    setup()
